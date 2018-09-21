1fb3ac8d73bf8fe871d5787232428093e19221b7
auxo <auxo@mail.ndmctsgh.edu.tw>
2018-09-21 19:24:59 +0800

    裝MvcSiteMapProvider.MVC5套件準備生麵包屑

--- Mod02_/Controllers/HomeController.cs
+++ Mod02_/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using Mod02_.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,41 +12,6 @@ namespace Mod02_.Controllers
         public ActionResult Index()
         {
             return View();
-        }
-        [HttpPost]
-        public ActionResult Index(FormCollection f)
-        {
-            ViewBag.id = f["id"];
-            ViewBag.name = f["name"];
-            return View();
-
-        }
-
-        public ActionResult Index2()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Index2(Member m)
-        {
-            ViewBag.id = m.id;
-            ViewBag.name = m.name;
-            return View();
-
-        }
-
-        public ActionResult Index3()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Index3(Member m)
-        {
-            ViewBag.id = m.id;
-            ViewBag.name = m.name;
-            ViewBag.phone = string.Join("_", m.phone);
-            return View();
-
         }
     }
 }
